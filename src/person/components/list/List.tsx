e2ed4a9d11b609d3a67e89afbd9523096fc86c68
DiogoMedina97 <diogo_medina97@hotmail.com>
2022-10-26 19:44:51 -0300

    feat(person): add list of people

--- src/person/components/list/List.tsx
+++ src/person/components/list/List.tsx
@@ -1,20 +1,52 @@
+import { useEffect, useState, useCallback } from 'react';
 import Table from 'react-bootstrap/Table';
 
-export const List = () => (
-  <Table striped bordered>
-    <thead>
-      <tr>
-        <th>ID</th>
-        <th>Nome</th>
-        <th>Sobrenome</th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr>
-        <td></td>
-        <td></td>
-        <td></td>
-      </tr>
-    </tbody>
-  </Table>
-);
+import { PersonEntityInterface } from '../../interface';
+import * as service from '../../service';
+
+export const List = () => {
+  interface ListState {
+    readonly isLoading: boolean;
+    readonly people?: PersonEntityInterface[];
+  }
+  const [state, setState] = useState<ListState>({ isLoading: false });
+
+  const loadData = useCallback(async () => {
+    setState((s) => ({ ...s, isLoading: true }));
+    const people = await service.find();
+    setState((s) => ({ ...s, isLoading: false, people }));
+  }, []);
+
+  useEffect(() => {
+    loadData().catch((err) => console.log(err));
+  }, [loadData]);
+
+  const { isLoading, people } = state;
+
+  if (isLoading) return <>Carregando...</>;
+
+  if (!people) return null;
+
+  return (
+    <Table striped bordered>
+      <thead>
+        <tr>
+          <th></th>
+          <th>ID</th>
+          <th>Nome</th>
+          <th>Sobrenome</th>
+        </tr>
+      </thead>
+      <tbody>
+        {people.map((person) => (          
+          <tr>
+            <td></td>
+            <td>{person.id}</td>
+            <td>{person.name}</td>
+            <td>{person.surname}</td>
+          </tr>
+        ))}
+      </tbody>
+    </Table>
+  );
+};
--- src/person/service.ts
+++ src/person/service.ts
@@ -5,6 +5,12 @@ import { PersonEntityInterface } from './interface';
 
 const { REACT_APP_API_URL='' } = process.env;
 
+
+export const find = async ()  => {
+  const res = await axios.get(`${REACT_APP_API_URL}/person`);
+  return res.data as PersonEntityInterface[];
+};
+
 export const create = async (dto: CreateDTO)  => {
   const res = await axios.post(`${REACT_APP_API_URL}/person`, dto);
   return res.data as PersonEntityInterface;
