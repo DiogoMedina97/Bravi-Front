e1f885c6debed38e845280279e0653094afa9282
DiogoMedina97 <diogo_medina97@hotmail.com>
2022-10-26 22:36:16 -0300

    feat: add link to header

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,6 @@ import { Router } from './Router';
 
 export const App = () => (
   <Container>
-    <h1 className="my-3">Bravi</h1>
     <Router />
   </Container>
 );
--- src/Router.tsx
+++ src/Router.tsx
@@ -1,9 +1,12 @@
-import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
+import { BrowserRouter, Route, Routes, Navigate, Link } from 'react-router-dom';
 
 import { PersonRouter } from './person/components/PersonRouter';
 
 export const Router = () => (
   <BrowserRouter>
+    <Link to="/">
+      <h1 className="my-3">Bravi</h1>
+    </Link>
     <Routes>
       <Route path="person/*" element={<PersonRouter />} />
       <Route path="/*" element={<Navigate to="/person" replace />} />
